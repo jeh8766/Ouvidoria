6c75a8801350ce107e451bf7b35fe61d31af9110
jeh8766 <jeh87662@gmail.com>
2024-11-20 23:48:31 -0300

    Menu

---
+++ arquivo1.py
@@ -0,0 +1,19 @@
+escolha = 1
+while escolha != 7:
+    print(
+        "\n|1| Listagem das Manifestações \n|2| Listagem de Manifestações por Tipo \n|3| Criar uma nova Manifestação\n|4| Exibir quantidade de manifestações \n|5| Pesquisar uma manifestação por código \n|6| Excluir uma Manifestação pelo Código \n|7| Sair do Sistema.")
+    escolha = int(input("Digite sua escolha: "))
+    if escolha == 1:
+
+    if escolha == 2:
+
+    if escolha == 3:
+
+    if escolha == 4:
+
+    if escolha == 5:
+
+    if escolha == 6:
+
+    if escolha != 7:
+
